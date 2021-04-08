e3f7102415c510686b0ee3cdf4d006085312e270
Egor Shulga <shulga_egyu@open.ru>
2021-04-08 16:47:58 +0300

    settings moved from .env to config

--- back-end/@config/index.ts
+++ back-end/@config/index.ts
@@ -1,11 +1,10 @@
-require('dotenv').config();
 import appRoot from 'app-root-path';
 
-export const PORT = process.env.PORT;
+export const PORT = 8877;
 export const ROOT_DIR = appRoot;
 export const VIEWS_DIR = `${ROOT_DIR}/views`;
 export const STATIC_PATH = '/static';
-export const MONGO_HOST = process.env.MONGO_HOST;
-export const MONGO_PORT = process.env.MONGO_PORT;
+export const MONGO_HOST = 'localhost';
+export const MONGO_PORT = '27017';
 export const DB_NAME = 'tree-view';
 export const MONGO_URI = `mongodb://${MONGO_HOST}:${MONGO_PORT}/${DB_NAME}`;
